27961d1c1273f34270faeab32d323dd63744c207
RexSteele <rexralphsteele@gmail.com>
2021-02-27 21:26:00 -0700

    first usage of cargo

---
+++ learningScripts/hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+/Users/rex/Documents/RC_Assignment/learningScripts/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
+
+/Users/rex/Documents/RC_Assignment/learningScripts/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ learningScripts/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/rex/Documents/RC_Assignment/learningScripts/hello_cargo/target/debug/hello_cargo: /Users/rex/Documents/RC_Assignment/learningScripts/hello_cargo/src/main.rs
